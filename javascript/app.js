74038b5be9606a6d1c4470f5485d2025b3665baf
Franco Sabio <francis.sabio.jr@gmail.com>
2018-07-08 15:16:53 -0400

    work in progress

---
+++ javascript/app.js
@@ -0,0 +1,13 @@
+var rap_questions = [{
+    question: "name that tune",
+    choices: ["all about the benjamins", "O.P.P", "C.R.E.A.M"],
+    correctAnswer: "O.P.P." 
+}, {
+    question: "name that tune",
+    choices: ["It Was a Good Day", "God's Plan", "Fight the Power"],
+    correctAnswer: "It was a Good Day"
+}, {
+    question: "name that tune",
+    choices: ["In Da Club", "Bodak Yellow", "California Love"],
+    correctAnswer: "California Love"
+}];
