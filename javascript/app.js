a9f68fbb8ea6a23f9beeec8ab09627f59f4c18f9
Franco Sabio <francis.sabio.jr@gmail.com>
2018-07-08 15:44:22 -0400

    work in progress

--- javascript/app.js
+++ javascript/app.js
@@ -11,3 +11,31 @@ var rap_questions = [{
     choices: ["In Da Club", "Bodak Yellow", "California Love"],
     correctAnswer: "California Love"
 }];
+
+var country_questions = [{
+    question: "name that tune",
+    choices: ["I Like It I love It", "Forever and Ever Amen", "Leaving on a Jet Plane"],
+    correctAnswer: "I Like It I Love It"
+}, {
+    question: "name that tune",
+    choices: ["Before He Cheats", "Stand By Your Man", "Redneck Woman"],
+    correctAnswer: "Stand By Your Man"
+}, {
+    question: "name that tune",
+    choices: ["Small Town Boy", "Five More Minutes", "Friends in Low Places"],
+    correctAnswer: "Friends in Low Places"
+}];
+
+var pop_questions = [{
+    question: "name that tune",
+    choices: ["Perfect", "Sorry", "Like I Love You"],
+    correctAnswer: "Perfect"
+}, {
+    question: "name that tune",
+    choices: ["Happy", "Finesse", "Love Yourself"],
+    correctAnswer: "Happy"
+}, {
+    question: "name that tune",
+    choices: ["Thinking Out Loud", "Hello", "When I Was Your Man"],
+    correctAnswer: "Thinking Out Loud"
+}];
