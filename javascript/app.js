41d067b373844ddbc3d6603f85006b58d5f2b3c4
Franco Sabio <francis.sabio.jr@gmail.com>
2018-07-09 12:00:22 -0400

    work in progress

--- javascript/app.js
+++ javascript/app.js
@@ -1,42 +1,42 @@
 var rap_questions = [{
-    song: src = "im_still_in_love_with_you.mp3",
-    choices: ["all about the benjamins", "O.P.P", "C.R.E.A.M"],
+    song: src = "OPP.mp3",
+    options: ["all about the benjamins", "O.P.P", "C.R.E.A.M"],
     correctAnswer: "O.P.P." 
 }, {
-    question: "name that tune",
-    choices: ["It Was a Good Day", "God's Plan", "Fight the Power"],
+    song: "It_Was_A_Good_Day.mp3",
+    options: ["It Was a Good Day", "God's Plan", "Fight the Power"],
     correctAnswer: "It was a Good Day"
 }, {
-    question: "name that tune",
-    choices: ["In Da Club", "Bodak Yellow", "California Love"],
+    song: "California_Love.mp3",
+    options: ["In Da Club", "Bodak Yellow", "California Love"],
     correctAnswer: "California Love"
 }];
 
 var country_questions = [{
-    question: "name that tune",
-    choices: ["I Like It I love It", "Forever and Ever Amen", "Leaving on a Jet Plane"],
+    song: "I_Like_It_I_Love_It.mp3",
+    options: ["I Like It I love It", "Forever and Ever Amen", "Leaving on a Jet Plane"],
     correctAnswer: "I Like It I Love It"
 }, {
-    question: "name that tune",
-    choices: ["Before He Cheats", "Stand By Your Man", "Redneck Woman"],
+    song: "Stand_By_Your_Man.mp3",
+    options: ["Before He Cheats", "Stand By Your Man", "Redneck Woman"],
     correctAnswer: "Stand By Your Man"
 }, {
-    question: "name that tune",
-    choices: ["Small Town Boy", "Five More Minutes", "Friends in Low Places"],
+    song: "Friends_In_Low_Places.mp3",
+    options: ["Small Town Boy", "Five More Minutes", "Friends in Low Places"],
     correctAnswer: "Friends in Low Places"
 }];
 
 var pop_questions = [{
-    question: "name that tune",
-    choices: ["Perfect", "Sorry", "Like I Love You"],
-    correctAnswer: "Perfect"
+    song: "Sorry.mp3",
+    options: ["Perfect", "Sorry", "Like I Love You"],
+    correctAnswer: "Sorry"
 }, {
-    question: "name that tune",
-    choices: ["Happy", "Finesse", "Love Yourself"],
+    song: "Happy.mp3",
+    options: ["Happy", "Finesse", "Love Yourself"],
     correctAnswer: "Happy"
 }, {
-    question: "name that tune",
-    choices: ["Thinking Out Loud", "Hello", "When I Was Your Man"],
+    song: "Thinking_Out_Loud.mp3",
+    options: ["Thinking Out Loud", "Hello", "When I Was Your Man"],
     correctAnswer: "Thinking Out Loud"
 }];
 
@@ -48,7 +48,25 @@ var r_song_01 = document.getElementById("sean_paul");
 var r_title = document.getElementById("rap_title");
 var r_answers = document.getElementById("r_answers");
 
+function setRapForm_01(){
+    let song= document.getElementById("sean_paul");
+    song.setAttribute("src", "OPP.mp3");
+    let radioInputs= document.querySelectorAll("#r_answers input");
+    radioInputs[0].value= "Franco";
+    let label = document.querySelectorAll("#r_answers label");
+    let span = label[0].querySelector("span");
+    span.innerText= "Franco";
+    // label[0].span[0].innerText= "Franco";
+    
+}
 
+setRapForm_01();
+
+function setRapForm_02(){
+    let song= document.getElementById("sean_paul");
+    song.setAttribute("src", "OPP.mp3");
+    let radioInputs= document.querySelectorAll("#r_answers input");
+}
 
 
 // myRapButton.addEventListener ("click", function(){
@@ -59,16 +77,16 @@ var r_answers = document.getElementById("r_answers");
 // document.getElementsByName("rap_questions");
 // });
 
-var myPopButton = document.getElementById("pop_button")
+var myPopButton = document.getElementById("pop_button");
 
 myPopButton.addEventListener ("click", function(){
     console.log("Like, really?");
     console.log(pop_questions[2].choices[1]);
 });
 
-var myCountryButton = document.getElementById("country_button")
+// var myCountryButton = document.getElementById("country_button");
 
-myCountryButton.addEventListener ("click", function(){
-    console.log("Country Folk");
-    console.log(country_questions[2].choices[1]);
-});
+// myCountryButton.addEventListener ("click", function(){
+//     console.log("Country Folk");
+//     console.log(country_questions[2].choices[1]);
+// });
