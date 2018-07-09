0369c93521f5af19afe57d9aa31a494c7badaf3d
Franco Sabio <francis.sabio.jr@gmail.com>
2018-07-09 16:56:15 -0400

    work in progress

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,6 @@
 var rap_questions = [{
     song: src = "OPP.mp3",
-    options: ["all about the benjamins", "O.P.P", "C.R.E.A.M"],
+    options: ["All About the Benjamins", "O.P.P.", "C.R.E.A.M."],
     correctAnswer: "O.P.P." 
 }, {
     song: "It_Was_A_Good_Day.mp3",
@@ -42,23 +42,34 @@ var pop_questions = [{
 
 // console.log(rap_questions[2].options[1]);
 
-// var myRapButton = document.getElementById("rap_button");
+var myRapButton = document.getElementById("rap_button");
 
-var r_song_01 = document.getElementById("sean_paul");
+// var r_song_01 = document.getElementById("sean_paul");
 var r_title = document.getElementById("rap_title");
 var r_answers = document.getElementById("r_answers");
 
 function appInit() {
-    var song= document.getElementById("rap_audio");
+    var song = document.getElementById("rap_audio");
     song.setAttribute("src", "OPP.mp3");
     var r_ans_01 = document.getElementById("answer_01_txt");
     var r_ans_01_val = document.getElementById("answer_01");
+        r_ans_01.innerHTML= rap_questions[0].options[0];
+        r_ans_01_val.setAttribute("value", rap_questions[0].options[0]);
+
     var r_ans_02 = document.getElementById("answer_02_txt");
-    var r_ans_02 = document.getElementById("answer_02");
-    var r_ans_03 = document.getElementById("answer_03_txt");
-    var r_ans_03 = document.getElementById("answer_03");
+    var r_ans_02_val = document.getElementById("answer_02");
+        r_ans_02.innerHTML = rap_questions[0].options[1];
+        r_ans_01_val.setAttribute("value", rap_questions[0].options[1]);
+
+    var r_ans_03 = document.getElementById("answer_03_txt"); 
+    var r_ans_03_val = document.getElementById("answer_03");
+        r_ans_03.innerHTML = rap_questions[0].options[2];
+        r_ans_03_val.setAttribute("value", rap_questions[0].options[2]);
+
     r_ans_01.innerHTML= rap_questions[0].options[0];
     r_ans_01_val.setAttribute("value", rap_questions[0].options[0]);
+    r_ans_02_val.setAttribute("value", rap_questions[0].options[1]);
+    r_ans_03_val.setAttribute("value", rap_questions[0].options[2]);
 }
 
 appInit();
@@ -84,19 +95,32 @@ function setRapForm_02(){
 }
 
 
-// myRapButton.addEventListener ("click", function(){
+myRapButton.addEventListener ("click", function(event){
+    event.preventDefault();
 //     document.getElementsByName("rap_questions").setAttribute[(question[0], choices[0]),
-//     (question[0], choices[1]),
-//     (question[0], choices[2]),
+//     (question[0], options[1]),
+//     (question[0], options[2]),
 //     document.getElementById("rap_title").innerHTML
 // document.getElementsByName("rap_questions");
-// });
+var rap_options = document.getElementsByClassName("hiphop");
+    // console.log(rap_options);
+    // console.log(rap_options[0].checked);
+for(i = 0; i < rap_options.length; i += 1){
+    console.log(rap_options[i].value);
+if (rap_options[i].value === "O.P.P" && rap_options[i].checked){
+    console.log("correct");
+}
+}
+});
+
+
 
 var myPopButton = document.getElementById("pop_button");
 
-myPopButton.addEventListener ("click", function(){
+myPopButton.addEventListener ("click", function(event){
+    console.log(event.target);
     console.log("Like, really?");
-    console.log(pop_questions[2].choices[1]);
+    console.log(pop_questions[2].options[1]);
 });
 
 // var myCountryButton = document.getElementById("country_button");
