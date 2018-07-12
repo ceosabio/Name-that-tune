81f81253479987729b67070b7978ba58386714c3
Javier Lopez <jlopez@tagworldwide.com>
2018-07-12 00:12:17 -0400

    Reorganizing file structre and starting app logic

---
+++ javascript/data_songs.js
@@ -0,0 +1,12 @@
+var songs = [
+	{"title": "California_Love", "file": "", "genre": "Hip Hop" },
+	{"title": "Friends_In_Low_Places", "file": "", "genre": "Country" },
+	{"title": "Happy", "file": "", "genre": "Pop" },
+	{"title": "i_like_it_i_love_it", "file": "", "genre": "Country" },
+	{"title": "im_still_in_love_with_you", "file": "", "genre": "Pop" },
+	{"title": "It_Was_A_Good_Day", "file": "", "genre": "Hip Hop" },
+	{"title": "OPP", "file": "", "genre": "Hip Hop" },
+	{"title": "sorry", "file": "", "genre": "Pop" },
+	{"title": "Stand_By_Your_Man", "file": "", "genre": "Country" },
+	{"title": "Thinking_Out_Loud", "file": "", "genre": "Pop" }
+];
