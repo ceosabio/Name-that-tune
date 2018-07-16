5fd6179db813742f43c829c98835d55c2d0b37b7
Javier Lopez <jlopez@tagworldwide.com>
2018-07-15 23:37:04 -0400

    Adding file names to songs data file and answer button event listeners

--- javascript/app.js
+++ javascript/app.js
@@ -1,19 +1,34 @@
-/* globals console */
+/* globals console, songs */
 var genre_select = document.getElementById("genre_select"),
 	genre_options = genre_select.getElementsByClassName("genre"),
 	song_audio = document.getElementById("song_audio"),
 	song_option = document.getElementsByClassName("song_option");
 
-var genre_songs = [], choices_array = [], songs_length = songs.length;
+var genre_selected, genre_songs = [], choices_array = [], songs_length = songs.length;
 // Loop index variables
-var a, b, c;
+var a, b, c, d, e;
+
+function song_answer(evt) {
+	var song_current_file = song_audio.src.split('/').pop();
+	var selected_answer = evt.target.innerHTML;
+	for ( e = 0; e < songs_length; e++ ) {
+		if ( songs[e].file === song_current_file ) {
+			if ( songs[e].title === selected_answer ) {
+				console.log("CORRECT");
+			} else {
+				console.log("WRONG");
+			}
+			return;
+		}
+	}
+}
 
 function song_title_choices(){
 	var choices_index = [0,1,2,3];
 	var choices_random_order = [];
 	var choice_array_pos = choices_index[Math.floor(Math.random()*choices_index.length)];
-	var choices_array = [genre_songs[0].title];
-	var choices_random_order = [choice_array_pos];
+	choices_array = [genre_songs[0].title];
+	choices_random_order = [choice_array_pos];
 	choices_index.splice(choices_index.indexOf(choice_array_pos), 1);
 	var options_ammount = 0;
 
@@ -37,18 +52,25 @@ function song_title_choices(){
 	}
 }
 
+function button_listener(){
+	for ( d = 0; d < song_option.length; d++ ){ 
+		song_option[d].addEventListener("click", song_answer);
+	}
+}
+
 function song_load(){
-	song_audio.src = "songs/"+genre_songs[0].title+".mp3";
+	song_audio.src = "songs/"+genre_songs[0].file;
 }
 
 // CREATE AN ARRAY OF SONGS BASED ON THE GENRE SELECTION
 function genre_selection(evt){
 	genre_songs = [];
-	if ( evt.target.innerHTML === "Random" ) {
+	genre_selected = evt.target.innerHTML;
+	if ( genre_selected === "Random" ) {
 		genre_songs = songs;
 	} else {
 		for (b = 0; b < songs_length; b++) {
-			if (songs[b].genre === evt.target.innerHTML ) {
+			if (songs[b].genre === genre_selected ) {
 				genre_songs.push(songs[b]);
 			}
 		}
@@ -65,7 +87,8 @@ for (a = 0; a < genre_options.length; a++) {
 }
 
 function init(){
-
+	console.log("INIT");
+	button_listener();
 }
 
 window.onload = init;
--- javascript/data_songs.js
+++ javascript/data_songs.js
@@ -1,12 +1,12 @@
 var songs = [
-	{"title": "California_Love", "file": "", "genre": "Hip Hop" },
-	{"title": "Friends_In_Low_Places", "file": "", "genre": "Country" },
-	{"title": "Happy", "file": "", "genre": "Pop" },
-	{"title": "i_like_it_i_love_it", "file": "", "genre": "Country" },
-	{"title": "im_still_in_love_with_you", "file": "", "genre": "Pop" },
-	{"title": "It_Was_A_Good_Day", "file": "", "genre": "Hip Hop" },
-	{"title": "OPP", "file": "", "genre": "Hip Hop" },
-	{"title": "sorry", "file": "", "genre": "Pop" },
-	{"title": "Stand_By_Your_Man", "file": "", "genre": "Country" },
-	{"title": "Thinking_Out_Loud", "file": "", "genre": "Pop" }
+	{"title": "California Love", "file": "California_Love.mp3", "genre": "Hip Hop" },
+	{"title": "Friends In Low Places", "file": "Friends_In_Low_Places.mp3", "genre": "Country" },
+	{"title": "Happy", "file": "Happy.mp3", "genre": "Pop" },
+	{"title": "I Like It, I Love It", "file": "i_like_it_i_love_it.mp3", "genre": "Country" },
+	{"title": "Im still In Love With You", "file": "im_still_in_love_with_you.mp3", "genre": "Pop" },
+	{"title": "It Was A Good Day", "file": "It_Was_A_Good_Day.mp3", "genre": "Hip Hop" },
+	{"title": "O.P.P.", "file": "OPP.mp3", "genre": "Hip Hop" },
+	{"title": "Sorry", "file": "sorry.mp3", "genre": "Pop" },
+	{"title": "Stand By Your Man", "file": "Stand_By_Your_Man.mp3", "genre": "Country" },
+	{"title": "Thinking Out Loud", "file": "Thinking_Out_Loud.mp3", "genre": "Pop" }
 ];
